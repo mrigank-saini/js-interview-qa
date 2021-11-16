bfc86b787ebdd1cf2069e147da1e69741f18abfc
mrigank-saini <33893636+mrigank-saini@users.noreply.github.com>
2021-11-16 22:59:59 +0530

    Create output.js

---
+++ output-QAs/coffeebeans-16-11-2021/output.js
@@ -0,0 +1,22 @@
+//Q.1
+var i=2;
+const array = [];
+for(let i=0;i<i;i++){
+	array.push(i+1);
+}
+console.log(array);
+
+//Q.2
+var a=5;
+function length() {
+	console.log(this.a);
+}
+
+var obj = {
+	a:10,
+  method: function(a){
+  	length();
+    arguments[0]();
+  }
+};
+obj.method(length,2);
