9e46b779c1cb35b3ff14708b6912034ca120f88b
Anonymous <33893636+mrigank-saini@users.noreply.github.com>
2022-01-08 22:16:31 +0530

    Create qa.js

---
+++ output-QAs/paytm-payments-bank/qa.js
@@ -0,0 +1,34 @@
+/*
+Q-1: Output Question - 
+*/
+console.log(' [1]');
+setTimeout(() => console.log(" [2]"), 0);
+setTimeout(() => console.log(" [3]"), 1);
+
+const p = Promise.resolve();
+for(let i = 0; i < 3; i++) {p.then(() => {
+    setTimeout(() => {
+        console.log(' [4]')
+        setTimeout(() => console.log(" [5]"), 0);
+        p.then(() => console.log(' [6]'));
+    }, 0);
+    console.log(" [7]");
+})}
+
+console.log(" [8]");
+
+/*
+Q-2: Add a method addEventListener to an array such that we can attach any event to it and pass a callback once the event is triggered
+Also, you should have a method pushWithEvent which when called, should push the given element in the array and trigger all the events that
+the array is attached to. In the following example, we are trying to add a push event on array and logging each item in it to the console.
+pushWithEvent get 5 as an input and pushes it to the array and trigger "push" event on it (I'm still confused whether interviewer said we need
+to call the event on every item of the array or only the item we are currently pushing to the array.
+*/
+array =[1,2,3];
+array.addEventListener('push', (item) => {console.log(item);
+})
+array.pushWithEvent(5);
+
+/*
+Q-3 - Web vitals (FTTP, FTTO etc.) explain those
+*/
