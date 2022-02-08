cd4cb4124f76a96adf9593421d23419ccea1c3aa
Anonymous <33893636+mrigank-saini@users.noreply.github.com>
2022-02-08 23:27:27 +0530

    Update problems.js

--- output-QAs/phonepe-04-02-2022/problems.js
+++ output-QAs/phonepe-04-02-2022/problems.js
@@ -1,4 +1,4 @@
-//Q.1.
+//Q-1
 const p = new Promise((resolve, reject) => {
 	console.log(1);
   setTimeout(() => {
@@ -11,3 +11,29 @@ setTimeout(() => console.log(3));
 p.then(() => console.log(4));
 setTimeout(() => console.log(5));
 
+//Q-2
+function doSomething(){
+	return new Promise(function(resolve, reject){
+		reject();
+		resolve();
+	});
+}
+
+let promise = doSomething();
+
+promise.then(function(){
+	console.log("Success 1");
+})
+	.then(function(){
+  	console.log("Success 2");
+  })
+  .catch(function(){
+  	console.log("Error 1");
+  })
+  .then(function(){
+  	console.log("Success 3");
+    return doSomething();
+  })
+  .finally(function(){
+  	console.log("Finally");
+  });
