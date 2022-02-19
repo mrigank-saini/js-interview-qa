b9f56d3e7e61b128d3ebda2f3665000321a6f3d6
Anonymous <33893636+mrigank-saini@users.noreply.github.com>
2022-02-19 23:02:48 +0530

    Create problems.js

---
+++ output-QAs/olx-19-02-2022/problems.js
@@ -0,0 +1,18 @@
+/*
+
+Q-1:
+Polyfill for Bind
+
+Q-2:
+Polyfill for Promise.allSetlled()
+
+Q-3:
+setTimeout(fn(){console.log("First"},0
+Promise.resolve(setTimeout(fn(){console.log("Second"},0)
+Ans - First Second 
+
+Q-4: Event Looping
+
+Q-5: Diff bw Cookies and Sessions
+
+*/
